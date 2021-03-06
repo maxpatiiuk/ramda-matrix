f481b7f8937766329889270c36c1c4a38926f558
Max Patiiuk <max@patii.uk>
2021-03-06 22:06:03 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2018,
+		project: './tsconfig.json',
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	rules: {
+	},
+	settings: {
+		propWrapperFunctions: [
+			// The names of any function used to wrap propTypes,
+			// e.g. `forbidExtraProps`. If this isn't set, any propTypes
+			// wrapped in a function will be skipped.
+			'forbidExtraProps',
+			{ property: 'freeze', object: 'Object' },
+		],
+	},
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# Ramda Matrices
+Ramda-based library for working with matrices in JavaScript
+
+## Documentation
+Documentation is located in `src/index.ts`
+
+## Features
+* Strongly-typed (TypeScript)
+* Almost purely functional
+* Tiny bundle size
